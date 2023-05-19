becb11b9eebf3b96b838cdf12b3c5f93198334a6
BrisaBarro <alumno@CI000000000000>
2023-05-19 05:28:46 -0300

    actualizo la imagen de logo nuevamente...
    Signed-off-by: BrisaBarr <BrisaBarro>
    Signed-off-by: BrisaBarr <alumno@CI000000000000>
    Signed-off-by: BrisaBarro <alumno@CI000000000000>

---
+++ practico4/tp4-grupo18/src/test/java/ar/edu/unju/fi/pv2023/practico41/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pv2023.practico41;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
