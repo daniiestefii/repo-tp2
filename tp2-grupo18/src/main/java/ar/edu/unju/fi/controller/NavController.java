eec597c91a25c13904c433ca0a7973ac260b2fb5
Rutmir <Rutmir@DESKTOP-52NDDDQ>
2023-05-21 16:35:13 -0300

    creacion de los layouts y funcionamiento de nav

---
+++ tp2-grupo18/src/main/java/ar/edu/unju/fi/controller/NavController.java
@@ -0,0 +1,39 @@
+package ar.edu.unju.fi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NavController {
+  
+	@GetMapping("/formulario")
+	public String getFormularioPage() {
+	   
+		return "Formulario";
+	}
+	@GetMapping("/index")
+	public String getIndexPage() {
+		
+		return "index";
+	}
+	@GetMapping("/productos")
+	public String getProductosPage() {
+		
+		return "Productos";
+	}
+	@GetMapping("/consejos")
+	public String getConsejosPage() {
+		
+		return "ConsejodeSalud";
+	}
+	@GetMapping("/servicios")
+	public String getServiciosPage() {
+		
+		return "TablaServicios";
+	}
+	@GetMapping("/sucursal")
+	public String getSucursalPage() {
+		
+		return "Sucursal";
+	}
+}
