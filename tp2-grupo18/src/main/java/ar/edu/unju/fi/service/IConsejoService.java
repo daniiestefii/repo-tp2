286f5da5bec5600315551e86f99fbc7c97e332b5
daniiestefii <dani.estefania9304@gmail.com>
2023-06-10 22:44:14 -0300

    Se agrego documentacion en serv-consejo

--- tp2-grupo18/src/main/java/ar/edu/unju/fi/service/IConsejoService.java
+++ tp2-grupo18/src/main/java/ar/edu/unju/fi/service/IConsejoService.java
@@ -5,16 +5,39 @@ import jakarta.validation.Valid;
 
 import java.util.List;
 
+/**
+ * @author DOrdonez
+ */
 public interface IConsejoService {
+    /**
+     * @method Devuelve una lista de todos los consejos existentes
+     */
     List<Consejo> getListaConsejo();
+
+
+    /**
+     * @method Guarda un nuevo consejo
+     */
     void guardar(@Valid Consejo consejo);
 
+    /**
+     * @method  Devuelve el consejo cuyo título coincide con el parámetro titulo.
+     */
     Consejo getBy(String titulo);
 
+    /**
+     * @method Modifica un consejo existente
+     */
     void modificar(Consejo consejo);
 
+    /**
+     * @method Elimina un consejo existente
+     */
     void eliminar(Consejo consejo);
 
+    /**
+     * @method Devuelve un nuevo objeto Consejo
+     */
     Consejo getConsejo();
 
 }
